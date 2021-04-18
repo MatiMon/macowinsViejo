18a1bd33234cc6421d7216000b5245475ddfe5b6
mati <matimon@frba.utn.edu.ar>
2021-04-18 16:16:28 -0300

    delegado el filtrado de las ventas de un dia, modificado el metodo gananciasDeUnDia

--- src/main/java/negocio/Comercio.java
+++ src/main/java/negocio/Comercio.java
@@ -4,13 +4,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Negocio {
+public class Comercio {
     List<Venta> ventas;
 
+    public List<Venta> ventasDeUnDia(LocalDateTime dia){
+        return ventas.stream().filter(venta -> venta.fecha.isEqual(dia)).collect(Collectors.toList());
+    }
+
     public double gananciasDeUnDia(LocalDateTime dia){
-        ventas.stream().filter(venta -> venta.fecha == dia).forEach(venta -> venta.prendas.stream().forEach(prenda -> venta.precioVenta(prenda).sum()));
-        /* y aca no se como hacer que por cada elemento de la coleccion ventas sume todos los precioVenta(Prenda prenda) de la coleccion prendas
-            quizas sea un error en el diseño del metodo precioVenta de la clase abstracta Venta pero no se me ocurrio otra forma
-        * */
+        return this.ventasDeUnDia(dia).stream().mapToDouble(venta -> venta.precioVenta()).sum();
     }
 }
