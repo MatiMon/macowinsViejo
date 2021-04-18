d2b51f252b1366898abf2c8079d9d23c3e1d2884
mati <matimon@frba.utn.edu.ar>
2021-04-18 16:10:34 -0300

    Renombrada la clase 'Negocio' a 'Comercio' para evitar confunsion con el paquete. Eliminada la funcion que retorna el tipo en Prenda

--- src/main/java/producto/Prenda.java
+++ src/main/java/producto/Prenda.java
@@ -9,18 +9,4 @@ public class Prenda {
         return precioPropio * estado.modificador();
     }
 
-    public String tipo(){
-        switch(tipo){
-            case 1: tipo.equals(Tipo.SACO);
-            return "Saco";
-            break;
-            case 2: tipo.equals(Tipo.PANTALON);
-            return "Pantalon";
-            break;
-            case 3: tipo.equals(Tipo.CAMISA);
-            return "Camisa";
-            break;
-        }
-    }
-
 }
